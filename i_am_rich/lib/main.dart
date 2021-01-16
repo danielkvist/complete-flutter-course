6bc3cb78c8112575ff2863dbc85d82e152c8fc0f
danielkvist <danielkvist@protonmail.com>
2021-01-16 09:12:36 +0100

    feat(iamrich): add hello world

---
+++ i_am_rich/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+// main() is the starting point for all our Flutter
+// apps
+void main() {
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: Text('Hello, world!'),
+      ),
+    ),
+  );
+}
